c56f37dbe0afa050eeb88b5097701108f0da5641
nfort <laikisounds@gmail.com>
2017-01-21 19:45:38 +0300

    Add handlebars and views

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
 
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status('404');
-    res.send('404 - Not Found');
+    res.render('400');
 });
 
 app.use(function(err, req, res, next) {
     console.log(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Internal Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
-   console.log(`Express has been start on http://localhost:${app.get('port')};\n
-                Press Ctrl + C for finish`);
+    console.log(`Express has been start on http://localhost:${app.get('port')};\nPress Ctrl + C for finish`);
 });
