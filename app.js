c9af9af4b9f7b406f5250d5d9d7adfbc8c8bb650
nfort <laikisounds@gmail.com>
2017-01-22 17:47:22 +0300

    Add tours

--- app.js
+++ app.js
@@ -5,13 +5,30 @@ const fortune = require('./utils/fortunes');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+app.disable('x-powered-by');
 
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', function(req, res) {
     res.render('home');
+});
+
+app.get('/tours', function(req, res) {
+    const context = {
+        currency: {
+            name: 'Доллары США',
+            abbrev: 'USD'
+        },
+        tours: [
+            { name: 'Река Худ', price: '$99.95' },
+            { name: 'Орегоун Коуст', price: '$159.95' }
+        ],
+        specialsUrl: './january-specials',
+        currencies: ['USD', 'GBP', 'BTC']
+    };
+    res.render('tours', context);
 });
 
 app.get('/about', function(req, res) {
@@ -31,5 +48,5 @@ app.use(function(err, req, res, next) {
 
 app.listen(app.get('port'), function() {
     console.log(`Express has been start on http://localhost:${app.get('port')};\n` +
-                'Press Ctrl + C for finish');
+        'Press Ctrl + C for finish');
 });
